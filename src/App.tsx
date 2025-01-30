aa720c3f9c3372fb6d2352050331d4dcefea08e0
ryoung610 <me0610@gmail.com>
2025-01-30 16:22:34 -0500

    added per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ const client = generateClient<Schema>();
 function App() {
 
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-    const { signOut } = useAuthenticator();
+    const { user ,signOut } = useAuthenticator();
+    
   
   function deleteTodo(id: string) {
     client.models.Todo.delete({ id })
@@ -26,7 +27,7 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
